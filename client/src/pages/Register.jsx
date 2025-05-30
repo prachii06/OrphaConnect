07780880d72e402b0ee576aab7bb7eaba8f24d1d
prachigohil-2006 <prachiigohil06@gmail.com>
2025-05-30 15:33:09 +0530

    solved by harshil

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -139,7 +139,7 @@ const Register = () => {
             whileTap={{ scale: 0.98 }}
             className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition-all"
           >
-            {loading ? 'Registering...' : 'Register'}
+            {loading ? 'Registering..' : 'Register'}
           </motion.button>
         </form>
       </motion.div>
